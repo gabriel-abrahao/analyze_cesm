d042257727bd2dfccb66d38915c32913e1409862
Gabriel Abrahao <gabrielmabrahao@gmail.com>
2021-02-22 13:38:19 -0300

    EDD computation

---
+++ ag_preprocess/compute_gs_vars_fixed_CESM.py
@@ -0,0 +1,363 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+import cftime
+import tqdm
+import os
+import re
+import copy
+import datetime
+import numba
+from numba import jit,prange
+
+# %% [markdown]
+# ### Setup
+
+# %%
+# infolder = "../external/data/xavier/daily_all/"
+baseinfolder = "../output_ag/preprocessed/"
+baseoutfolder = "../output_ag/dd_calc/"
+
+# models      =   ["BCC-CSM2-MR","CanESM5","EC-Earth3-Veg","GFDL-CM4","MRI-ESM2-0"]
+# scens       =   ["historical", "ssp245", "ssp585"]
+scens       =   next(os.walk(baseinfolder))[1]
+syears      =   [2006 for i in scens]
+eyears		=	[2050 for i in scens]
+# syears      =	[2000, 2016, 2016]
+# eyears		=	[2014, 2051, 2051]
+
+crops = ["single","maize"]
+
+calname = "ming595_mean"
+
+temppref = "temp.daily."
+tmaxpref = "tmax.daily."
+tminpref = "tmin.daily."
+precpref = "prec.daily."
+vpdpref = "vpd.daily."
+
+# calfolder = "fixed_calendars/"+calname+"/"
+calfolder = "../input/calendars/"+calname+"/"
+calsuf = ".crop.calendar.fill.nc"
+planvar = "plant.start"
+harvvar = "harvest.end"
+
+# Parameters for evaluating the temperature distribution
+# Tlo = -5.0
+Tlo = 10.0
+# Thi = 45.0
+Thi = 32.0
+Tint = 1.0
+
+
+
+
+# %% [markdown]
+# ### Defining functions
+
+# %%
+# Opens and concatenates a harvest year with the equivalent planting year
+def concat_clim(infolder,climpref,hyear):
+    pyear = hyear - 1
+
+    climharr = xr.open_dataarray(infolder+climpref+str(hyear)+".nc")
+    climparr = xr.open_dataarray(infolder+climpref+str(pyear)+".nc")
+
+    climarr = xr.concat([climparr,climharr], dim = "time")
+    return climarr
+
+# Opens two years (planting and harvest) given a folder, a prefix and a harvest year
+def read_cfnoleap_year(infolder,climpref,hyear):
+
+    timerange = xr.cftime_range(cftime.DatetimeNoLeap(hyear-1, 1, 1, 0, 0, 0, 0),cftime.DatetimeNoLeap(hyear, 12, 31, 0, 0, 0, 0))
+
+    climarr = xr.open_dataarray(infolder + "/" + climpref + "allyears.nc").sel(time = timerange)
+    return climarr
+
+
+# %%
+# Calculates t distribution for a single day
+@jit(nopython=True)
+def calc_dist_day(Tmin,Tmax,Tl1s,Tint):
+    res = 0.005 #Resolution (dt, in days) on which to evaluate the T sine curve
+
+#     Tl1s = np.arange(Tlo,Thi,Tint)
+    nT = Tl1s.shape[0]
+    exps = np.zeros_like(Tl1s)
+
+    t = np.arange(0,1,res)
+    nt = t.shape[0]
+
+    Tamp = (Tmax-Tmin)/2.0
+    Tmed = (Tmax+Tmin)/2.0
+
+    T = Tmed + Tamp*np.sin(t*(2.0*np.pi/1))
+
+    for tcount in range(nT):
+        Tl1 = Tl1s[tcount]
+        Tl2 = Tl1 + Tint
+        exps[tcount] = np.sum(np.invert(np.isnan(
+            np.where((T>=Tl1) & (T<=Tl2) ,T,np.nan)
+        )))/nt
+    return exps
+
+
+@jit(nopython=True)
+def calc_dist_point(tmaxvec,tminvec,Tlos,Tint):
+    allexps = np.zeros_like(Tlos)
+    for day in range(tminvec.shape[0]):
+        allexps = allexps + calc_dist_day(tminvec[day],tmaxvec[day],Tlos,Tint)
+    return allexps
+
+
+# %%
+# Calculates GDD for a single day
+@jit(nopython=True)
+def calc_gdd_day(Tmin,Tmax,Tl1s):
+    res = 0.005 #Resolution (dt, in days) on which to evaluate the T sine curve
+
+    #     Tl1s = np.arange(Tlo,Thi,Tint)
+    nT = Tl1s.shape[0]
+    gdds = np.zeros_like(Tl1s)
+
+    t = np.arange(0,1,res)
+    nt = t.shape[0]
+
+    Tamp = (Tmax-Tmin)/2.0
+    Tmed = (Tmax+Tmin)/2.0
+
+    T = Tmed + Tamp*np.sin(t*(2.0*np.pi/1))
+
+    for tcount in range(nT):
+        Tl1 = Tl1s[tcount]
+        gdds[tcount] = np.sum(np.invert(np.isnan(
+            np.where((T>=Tl1),T,np.nan)
+        ))*(T-Tl1))/nt
+
+    return gdds
+
+@jit(nopython=True)
+def calc_gdd_point(tmaxvec,tminvec,Tlos):
+    allgdds = np.zeros_like(Tlos)
+    for day in range(tminvec.shape[0]):
+        allgdds = allgdds + calc_gdd_day(tminvec[day],tmaxvec[day],Tlos)
+    return allgdds
+
+
+# %%
+# # Calculates VPD for a vector
+# # Assuming sea level pressure everywhere
+# @jit(nopython=True)
+# def calc_vpd(q,tmin,tmax):
+#     e = 1013.25 *(q/(1-q))/(0.622 + (q/(1-q)))
+
+#     esmin = 6.1078*np.exp((17.3*tmin)/(237.3+tmin))
+#     esmax = 6.1078*np.exp((17.3*tmax)/(237.3+tmax))
+#     es = (esmin+esmax)/2.0
+
+#     vpd = es - e
+#     vpd = np.where(vpd < 0,0,vpd)
+
+
+# %%
+# Calculates everything for the growing season given numpy arrays. 
+# Loops throught the points and calculates both distribution and regular gs means
+# Compiles with Numba parallel
+@jit(nopython=True, parallel = True)
+# @jit(nopython=True)
+def calc_all(planmat,harvmat,tempmat,tmaxmat,tminmat,precmat,
+             vpdmat,
+             tempmeanmat,tmaxmeanmat,tminmeanmat,precmeanmat,
+             vpdmeanmat,
+             trngmeanmat,ndaymat,
+             tempdistmat,tempgddsmat):
+    
+    def calc_vpd(q,tmin,tmax):
+        e = 1013.25 *(q/(1-q))/(0.622 + (q/(1-q)))
+
+        esmin = 6.1078*np.exp((17.3*tmin)/(237.3+tmin))
+        esmax = 6.1078*np.exp((17.3*tmax)/(237.3+tmax))
+        es = (esmin+esmax)/2.0
+
+        vpd = es - e
+        vpd = np.where(vpd < 0,0,vpd)
+        return vpd
+    
+    for lati in prange(tempmat.shape[1]):
+        for lonj in range(tempmat.shape[2]):
+            if (np.isnan(planmat[lati,lonj])) or (np.isnan(tempmat[0,lati,lonj])):
+                continue
+            plan = int(planmat[lati,lonj])
+            harv = int(harvmat[lati,lonj])
+
+            tempvec = tempmat[plan:harv,lati,lonj]
+            tmaxvec = tmaxmat[plan:harv,lati,lonj]
+            tminvec = tminmat[plan:harv,lati,lonj]
+            precvec = precmat[plan:harv,lati,lonj]
+            vpdvec = vpdmat[plan:harv,lati,lonj]
+            
+#             vpdvec = calc_vpd(sphmvec,tminvec,tmaxvec)
+
+            tempmeanmat[lati,lonj] = np.nanmean(tempvec)
+            tmaxmeanmat[lati,lonj] = np.nanmean(tmaxvec)
+            tminmeanmat[lati,lonj] = np.nanmean(tminvec)
+            precmeanmat[lati,lonj] = np.nanmean(precvec)
+            
+            vpdmeanmat[lati,lonj] = np.nanmean(vpdvec)
+            
+            trngmeanmat[lati,lonj] = np.nanmean(tmaxvec - tminvec)
+            ndaymat[lati,lonj] = np.int64(harv-plan)
+            # tempdistmat[:,lati,lonj] = calc_dist_point(tmaxvec,tminvec,Tlos,Tint)
+            tempgddsmat[:,lati,lonj] = calc_gdd_point(tmaxvec,tminvec,Tlos)
+
+# %% [markdown]
+# ### Begin main script
+
+# %%
+# iscen = 0
+for iscen in range(len(scens)):
+    scen = scens[iscen]
+    syear = syears[iscen]
+    eyear = eyears[iscen]
+    print(scen)
+
+    infolder = baseinfolder + scen + "/"
+    outfolder = baseoutfolder + "/" + calname + "/" + scen + "/"
+
+    # The basis of calculation will be harvest years
+    hyears = list(range(syear,eyear+1))
+
+
+    # Create output folder
+    if not os.path.exists(outfolder): os.makedirs(outfolder, exist_ok=True)
+
+    #FIXME: Loop crops here
+    # crop = crops[0]
+    for crop in crops:
+        print(crop)
+
+        # Open calendar and convert it to two-year based indexes. FIXME: Ignoring leap years here
+        caldata = xr.open_dataset(calfolder+crop+calsuf)
+        # planarr = caldata[planvar]
+        # harvarr = caldata[harvvar]
+        # harvarr = xr.where(harvarr < planarr,harvarr + 365,harvarr) - 1
+        planarr = caldata[planvar]
+        harvarr = caldata[harvvar]
+        planarr = xr.where(planarr < 213, planarr + 365, planarr)
+        harvarr = xr.where(harvarr < 213, harvarr + 365, harvarr)
+        harvarr = xr.where((harvarr - planarr) < 90, planarr + 90,harvarr) - 1 
+        harvarr.name = caldata[harvvar].name
+        xr.merge([planarr,harvarr]).to_netcdf(outfolder+"aux.calendar."+".nc")
+        
+
+        #FIXME: Loop here
+        # hyear = hyears[0]
+        for hyear in tqdm.tqdm(hyears):
+            # # These are for yearly calendars
+            # planarr = caldata[planvar].sel(year = hyear)
+            # harvarr = caldata[harvvar].sel(year = hyear)
+            # harvarr = xr.where(harvarr < planarr,harvarr + 365,harvarr) - 1 
+
+            # Open the climate arrays, concatenating them
+            temparr = read_cfnoleap_year(infolder,temppref,hyear)
+            tmaxarr = read_cfnoleap_year(infolder,tmaxpref,hyear)
+            tminarr = read_cfnoleap_year(infolder,tminpref,hyear)
+            precarr = read_cfnoleap_year(infolder,precpref,hyear)
+            vpdarr  = read_cfnoleap_year(infolder,vpdpref,hyear)
+
+            # Make sure the calendar arrays match the coordinates
+            # FIXME: This could be done only once, but then we would have to read 
+            # a climate array beforehand
+            if not (planarr["lat"].equals(temparr["lat"]) and planarr["lon"].equals(temparr["lon"]) ):
+                planarr = planarr.interp_like(temparr)
+                harvarr = harvarr.interp_like(temparr)
+
+
+            # Generate a vector of lower T bounds to use as metadata and speed up computation
+            Tlos = np.arange(Tlo,Thi,Tint)
+
+            # Preallocate the arrays that will be filled
+            lldims = ("lat","lon")
+            coords = [(i,temparr.coords[i].data,temparr.coords[i].attrs) for i in lldims] # Tuples with lat and lon dimension specs
+
+            # 2D arrays
+            tempmean = xr.DataArray(coords = coords, name = "tempmean")
+            tmaxmean = xr.DataArray(coords = coords, name = "tmaxmean")
+            tminmean = xr.DataArray(coords = coords, name = "tminmean")
+            precmean = xr.DataArray(coords = coords, name = "precmean")
+            vpdmean = xr.DataArray(coords = coords, name = "vpdmean")
+
+
+            trngmean = xr.DataArray(coords = coords, name = "trngmean")
+
+            ndayarr = xr.DataArray(coords = coords, name = "ndays")
+
+            # 2D + tmp arrays
+            tmp = ("tmp",Tlos,{"long_name":"Temperature interval lower bound","units":"degC"})
+            coords3d = [tmp] + coords
+            tempdist = xr.DataArray(np.nan, coords = coords3d, name = "tempdist")
+            tempgdds = xr.DataArray(np.nan, coords = coords3d, name = "tempgdds")
+
+            # This basically creates pointers to the numpy arrays inside the xr.Dataarrays
+            # We need those for numba to work. An alternative would be passing the .data in the function call
+            planmat = planarr.data
+            harvmat = harvarr.data
+
+            tempmat = temparr.data
+            tmaxmat = tmaxarr.data
+            tminmat = tminarr.data
+            precmat = precarr.data
+            vpdmat = vpdarr.data
+
+            tempmeanmat = tempmean.data
+            tmaxmeanmat = tmaxmean.data
+            tminmeanmat = tminmean.data
+            precmeanmat = precmean.data
+            vpdmeanmat = vpdmean.data
+
+            trngmeanmat = trngmean.data
+
+            ndaymat = ndayarr.data
+
+            tempdistmat = tempdist.data
+            tempgddsmat = tempgdds.data
+
+
+            # Calculates everything.
+            # FIXME: In this version, tempdistmat doesn't get filled
+            calc_all(planmat,harvmat,tempmat,tmaxmat,tminmat,precmat,
+                    vpdmat,
+                    tempmeanmat,tmaxmeanmat,tminmeanmat,precmeanmat,
+                    vpdmeanmat,
+                    trngmeanmat,ndaymat,
+                    tempdistmat,tempgddsmat)
+
+            
+            # Merge everything in a single Dataset
+            # outdata = xr.merge([tempmean,tmaxmean,tminmean,precmean,vpdmean,trngmean,ndayarr,tempdist,tempgdds])
+            outdata = xr.merge([tempmean,tmaxmean,tminmean,precmean,vpdmean,trngmean,ndayarr,tempgdds])
+            outdata.attrs['Crop'] = crop
+            outdata.attrs['harvest_year'] = hyear
+            outdata.attrs['calendar_path'] = calfolder+crop+calsuf
+            outdata.attrs['climdata_path'] = infolder
+            outdata.attrs['climdata_ex_path'] = infolder+temppref+str(hyear)+".nc"
+
+            # Write output
+            outfname = outfolder + crop + ".computed." + str(hyear) + ".nc"
+    #         outdata.to_netcdf(outfname)
+            outdata.to_netcdf(outfname,
+                    engine = "netcdf4",
+                    encoding = {"tempgdds":{'zlib': True, 'complevel': 1}} )
+
+
+# %%
+
+
+
+# %%
+
+
---
+++ ag_preprocess/preprocess_daily_v1.py
@@ -0,0 +1,235 @@
+# %%
+# from IPython import get_ipython
+
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+import os
+import re
+import tqdm
+import copy
+
+
+# xr.set_options(display_style='html')
+# get_ipython().run_line_magic('matplotlib', 'inline')
+# get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'retina'")
+
+# %%
+infolder = "../input/rcp_daily/"
+outfolder = "../output_ag/preprocessed/"
+varnamedict = {
+    "TREFHT" : "temp",
+    "TREFHTMX" : "tmax",
+    "TREFHTMN" : "tmin",
+    "QREFHT" : "sphum",
+    "PRECT" : "prec"
+}
+tempvarnames = ["temp", "tmax", "tmin"]
+precvarname = "prec"
+sphumvarname = "sphum"
+vpdvarname = "vpd"
+
+keepnames = tempvarnames + [precvarname] + [sphumvarname] + [vpdvarname]
+
+topofname = "topo.nc"
+reffname = "refgrid.nc"
+
+# models = ['BCC-CSM2-MR', 'CanESM5', 'EC-Earth3-Veg', 'GFDL-CM4', 'MRI-ESM2-0']
+
+# scens = ['historical','ssp245','ssp585']
+scens = os.listdir(infolder)
+
+# Domain, crop to Brazil
+minlat = -35
+maxlat = 7
+minlon = 287
+maxlon = 328
+
+#%%
+# Longitude flipping functions
+def get_lon_name(d):
+    if ('longitude' in d.dims):
+        lonname = 'longitude'
+    elif ('lon' in d.dims):
+        lonname = 'lon'
+    else:
+        raise
+    return lonname
+
+# Flipped here means that it goes from -180 to 180
+def global_is_lon_flipped(d):
+    lonname = get_lon_name(d)
+    return not not np.any(d[lonname]<0.0).data # The double negation converts it to a python boolean
+
+# Just works from a nonflipped (0:360) global grid
+def global_lon_flip(d):
+    lonname = get_lon_name(d)
+    nlon = d[lonname].size
+    dflip = copy.deepcopy(d.roll(lon=int(nlon/2),roll_coords=False))
+    # dflip[lonname] = dflip[lonname] - 180.0
+    dflip = dflip.assign_coords({lonname : dflip[lonname] - 180.0})
+    dflip = dlip.assign_
+    return dflip
+
+# 
+def global_lon_unflip(d):
+    lonname = get_lon_name(d)
+    nlon = d[lonname].size
+    dflip = copy.deepcopy(d.roll(lon=int(nlon/2),roll_coords=False))
+    # dflip[lonname] = dflip[lonname] + 180.0
+    dflip = dflip.assign_coords({lonname : dflip[lonname] + 180.0})
+    return dflip
+
+def brazil_lon_unflip(d):
+    dflip = copy.deepcopy(d)
+    lonname = get_lon_name(d) 
+    # lons = d[lonname].data
+    # dflip[lonname].data = lons-360
+    dflip = dflip.assign_coords({lonname : dflip[lonname] - 360.0})
+    return dflip
+
+# %%
+
+# Read topography and reference grid
+topo = xr.open_dataarray(topofname)
+refgrid = xr.open_dataset(reffname)
+refgrid = refgrid[[i for i in refgrid.data_vars.keys()][0]]
+try:
+    refgrid = refgrid.isel(time = 0)
+except: None
+
+
+
+# allfiles = os.listdir(infolder)
+
+invarnames = list(varnamedict.keys())
+
+#%%
+
+# scen = scens[0]
+# scen = "historical"
+# model = "BCC-CSM2-MR"
+# model = "MRI-ESM2-0"
+
+for scen in scens:
+    print(scen)
+
+    # Dataset that will contain all variables
+    bigds = xr.open_mfdataset(infolder + "/" + scen + "/run/*.nc")[invarnames]
+
+    # Crop to domain
+    bigds = bigds.sel(lat = slice(minlat,maxlat), lon = slice(minlon,maxlon))
+
+    # Rename variables
+    bigds = bigds.rename(varnamedict)
+
+
+    # bigds = xr.Dataset()
+
+    # # invarname = "tasmax"
+    # for invarname in invarnames:
+
+    #     r = re.compile(invarname+"_day_"+model+"_"+scen+".*")
+    #     varfiles = [infolder + f for f in allfiles if r.match(f)]
+    #     if len(varfiles) == 1:
+    #         varfname = varfiles[0]
+    #     else:
+    #         print("ERROR: More than one file matches the pattern")
+    #         print(r)
+    #         print(allfiles)
+
+    #     invards = xr.open_dataset(varfname)
+    #     # invards = xr.open_mfdataset(varfiles, combine = "by_coords")
+    #     # invards = invards.drop(["time_bnds","lat_bnds","lon_bnds"])
+
+
+    #     # Try removing metadata that might not be consistent
+    #     # such as bounds and "height" coordinate attributes
+    #     try:
+    #         del(invards.time.attrs["bounds"])
+    #     except: None
+    #     try:
+    #         del(invards.lat.attrs["bounds"])
+    #     except: None
+    #     try:
+    #         del(invards.lon.attrs["bounds"])
+    #     except: None
+    #     try:
+    #         del(invards[invarname].attrs["coordinates"])
+    #     except: None         
+    #     try: 
+    #         invards = invards.drop(["time_bnds","lat_bnds","lon_bnds"])
+    #     except: None      
+        
+    #     # Rename
+    #     invards = invards.rename({invarname:varnamedict[invarname]})
+
+    #     bigds = xr.merge([bigds,invards])
+
+    # # Get global attributes from the last DataSet, and remove variable_id
+    # bigds.attrs = invards.attrs
+    # del(bigds.attrs["variable_id"])
+
+    #%%
+
+    # Temperature variables
+    for tempvar in tempvarnames:
+        meta = copy.deepcopy(bigds[tempvar].attrs)
+        bigds[tempvar] = bigds[tempvar] - 273.15
+        bigds[tempvar].attrs = meta
+        bigds[tempvar].attrs["units"] = "degC"
+
+    ## %%
+    # Precipitation
+    meta = copy.deepcopy(bigds[precvarname].attrs)
+    bigds[precvarname] = bigds[precvarname] * 86400000.0
+    bigds[precvarname].attrs = meta
+    bigds[precvarname].attrs["units"] = "mm day-1"
+
+    # %% VPD
+
+    # Interpolate topography to generate a pressure field
+    if global_is_lon_flipped(topo) != global_is_lon_flipped(bigds):
+        topo = global_lon_unflip(topo)
+    topo = topo.interp_like(bigds)
+    pres = 1013.25*np.exp((-1)*(1.602769777072154)*np.log((np.exp(topo/10000.0)*213.15+75.0)/288.15))
+
+    bigds["estmax"]=6.1078*np.exp((17.3*bigds["tmax"])/(237.3+bigds["tmax"]))
+    bigds["estmin"]=6.1078*np.exp((17.3*bigds["tmin"])/(237.3+bigds["tmin"]))
+    bigds["es"]=(bigds["estmax"]+bigds["estmin"])/2
+    bigds["r"] = bigds["sphum"]/(1-bigds["sphum"])
+    bigds["e"] = (bigds["r"]*pres)/(0.622+bigds["r"])
+    bigds["rh"] = 100.0*bigds["e"]/bigds["es"]
+    bigds[vpdvarname] = bigds["es"] - bigds["e"]
+    bigds[vpdvarname] = bigds[vpdvarname].where(bigds[vpdvarname] >= 0,0) # Cap at zero
+
+    bigds[vpdvarname].attrs["long_name"] = "Vapour pressure deficit"
+    bigds[vpdvarname].attrs["units"] = "hPa"
+    bigds[vpdvarname]
+
+    # %%
+    # Drop intermediate variables
+    bigds = bigds.drop([i for i in bigds.data_vars.keys() if i not in keepnames])
+
+    # # Rename coordinates to conform with Xavier
+    # if ('lon' in bigds.dims) and ('lat' in bigds.dims):
+    #     bigds = bigds.rename({'lon':'longitude', 'lat': 'latitude'}) # some models labelled dimensions differently...
+
+    # # Unflip longitudes to be -180:180 like Xavier
+    # bigds = brazil_lon_unflip(bigds)
+
+    # # %%
+    # outds = bigds.interp_like(refgrid)
+    outds = bigds
+
+    #%%
+    print("Writing variables...")
+    for varname in tqdm.tqdm(keepnames):
+        scenoutfolder = outfolder + "/" + scen + "/"
+        # Create output folder
+        if not os.path.exists(scenoutfolder): os.makedirs(scenoutfolder, exist_ok=True)
+
+        outfname = scenoutfolder + varname + ".daily.allyears.nc"
+        outds[varname].to_netcdf(outfname)
+    # %%
